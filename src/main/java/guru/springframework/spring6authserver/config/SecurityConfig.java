2ee9127dea0898f07a9911f8d157694f89fd0be0
Miguel de la Ossa <miguel.ossa.abellan@gmail.com>
2025-07-04 18:38:39 +0100

    Updates for Docker

--- src/main/java/guru/springframework/spring6authserver/config/SecurityConfig.java
+++ src/main/java/guru/springframework/spring6authserver/config/SecurityConfig.java
@@ -81,16 +81,12 @@ public class SecurityConfig {
 
     @Bean
     @Order(3)
-    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
-            throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((authorize) -> authorize
+                .authorizeHttpRequests(authorize -> authorize
                         .anyRequest().authenticated()
                 )
-                // Form login handles the redirect to the login page from the
-                // authorization server filter chain
-                .formLogin(Customizer.withDefaults());
-
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
         return http.build();
     }
 
@@ -156,7 +152,9 @@ public class SecurityConfig {
 
     @Bean
     public AuthorizationServerSettings authorizationServerSettings() {
-        return AuthorizationServerSettings.builder().build();
+        return AuthorizationServerSettings.builder()
+                .issuer("http://auth-server:9000")
+                .build();
     }
 
 }
